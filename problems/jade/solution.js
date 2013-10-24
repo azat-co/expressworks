e0562559ca0c466036bef45a357d26a2ac2ea43a
Azat Mardanov <azat.mardanov@docusign.com>
2013-10-24 15:07:10 -0700

    add jade exersice

---
+++ problems/jade/solution.js
@@ -0,0 +1,9 @@
+var path = require('path')
+var express = require('express')
+var app = express()
+app.set('view engine', 'jade')
+app.set('views', process.argv[3])
+app.get('/home', function(req, res) {
+  res.render('index', {date: new Date().toDateString()})
+})
+app.listen(process.argv[2])
