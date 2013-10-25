2939281b0bccebd3c3c3455d9711270af7bc742d
Azat Mardanov <azat.mardanov@docusign.com>
2013-10-24 18:12:09 -0700

    add static and stylus exercises

---
+++ problems/stylish_css/public/index.html
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>expressworks</title>
+    <link rel="stylesheet" type="text/css" href="/main.css"/>
+  </head>
+  <body>
+    <p>I am red!</p>
+  </body>
+</html>
---
+++ problems/stylish_css/solution.js
@@ -0,0 +1,9 @@
+var path = require('path')
+var express = require('express')
+var app = express()
+
+app.use(require('stylus').middleware(process.argv[3]));
+app.use(express.static(process.argv[3]));
+
+
+app.listen(process.argv[2])
