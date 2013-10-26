f5ea0e022928746a2798a5435e22db3d8a82508a
Gergely Nemeth <mail@nemethgergely.com>
2013-10-26 13:53:29 +0200

    linted hello world

--- problems/hello_world/solution.js
+++ problems/hello_world/solution.js
@@ -1,6 +1,11 @@
-var express = require('express')
-var app = express()
-app.get('/home', function(req, res) {
-  res.end('Hello World!')
-})
-app.listen(process.argv[2])
+/*global process, require*/
+/*jslint unparam:true*/
+(function () {
+    'use strict';
+    var express = require('express'),
+        app = express();
+    app.get('/home', function (req, res) {
+        res.end('Hello World!');
+    });
+    app.listen(process.argv[2]);
+}());
