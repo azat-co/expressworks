ee5e4cde5a540bbe74641ee23681a42fd7089d68
Justin Porter <justinrporter@gmail.com>
2015-05-16 13:02:05 -0500

    Update json_me solution.js
    res.send(500) -> res.sendStatus(500) to clear warning that "express deprecated res.send(status): Use res.sendStatus(status) instead".

--- exercises/json_me/solution/solution.js
+++ exercises/json_me/solution/solution.js
@@ -5,11 +5,11 @@ var fs = require('fs')
 app.get('/books', function(req, res){
   var filename = process.argv[3]
   fs.readFile(filename, function(e, data) {
-    if (e) return res.send(500)
+    if (e) return res.sendStatus(500)
     try {
       books = JSON.parse(data)
     } catch (e) {
-      res.send(500)
+      res.sendStatus(500)
     }
     res.json(books)
   })
