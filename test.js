9799d4b460e6ebcee43cc6ddaf62e00ff21a2709
Azat Mardan <hi@azat.co>
2015-12-12 21:53:34 -0800

    add changes :last_quarter_moon_with_face:

--- test.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-var port = process.argv[2]
-var staticFilePath = process.argv[3]
-var app = express()
-
-app.use(express.static(staticFilePath))
-
-app.listen(port)
