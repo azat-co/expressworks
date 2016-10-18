0171483ff0c8a284e498f175c2c91f04701e3a96
Elias Meire <me@eliasmei.re>
2016-10-19 00:45:39 +0200

    Replace jade by pug in excercise files

--- exercises/pug/solution/solution.js
+++ exercises/pug/solution/solution.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express()
-app.set('view engine', 'jade')
+app.set('view engine', 'pug')
 app.set('views', process.argv[3])
 app.get('/home', function(req, res) {
   res.render('index', {date: new Date().toDateString()})
