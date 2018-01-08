1c198016ec836f0a3891a82abaaf5b8561f92032
Travis Ali <ali.gilgil1@gmail.com>
2018-01-08 13:43:35 -0500

    Removed semicolons for consistency

--- exercises/stylish_css/solution/solution.js
+++ exercises/stylish_css/solution/solution.js
@@ -1,8 +1,7 @@
 var express = require('express')
 var app = express()
 
-app.use(require('stylus').middleware(process.argv[3]));
-app.use(express.static(process.argv[3]));
-
+app.use(require('stylus').middleware(process.argv[3]))
+app.use(express.static(process.argv[3]))
 
 app.listen(process.argv[2])
